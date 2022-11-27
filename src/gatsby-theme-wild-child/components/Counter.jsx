5caf3ca2b1131fa61299fb6bc0131ac790b40369
andy-dextrous <marketing@andrewscrivens.com.au>
2022-11-27 13:08:04 +1000

    Update Counter.jsx

--- src/gatsby-theme-wild-child/components/Counter.jsx
+++ src/gatsby-theme-wild-child/components/Counter.jsx
@@ -9,15 +9,37 @@ function Counter({
   details = "impressions",
   children,
   value = "",
+  increment = 1,
 }) {
   const counter = React.useRef(0)
 
   useEffect(() => {
-    gsap.effects.counter(counter.current, {
-      end: end,
-      duration: 10,
-      scrollTrigger: { trigger: counter.current, start: "top 80%" },
-    })
+    let tl = gsap.timeline()
+    let num = counter.current.innerText.replace(/\,/g, "")
+    counter.current.innerText = num
+
+    tl.to(
+      counter.current,
+      {
+        duration: 10,
+        innerText: end,
+        modifiers: {
+          innerText: function (innerText) {
+            return gsap.utils
+              .snap(increment, innerText)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+          },
+        },
+        ease: "Power2.easeOut",
+        scrollTrigger: {
+          trigger: counter.current,
+          start: "top 80%",
+          toggleActions: "play none none none",
+        },
+      },
+      0
+    )
   }, [])
 
   return (

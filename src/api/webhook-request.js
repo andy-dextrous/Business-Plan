ee5cb958594904af3711c3c1fa2d06dc84abd03b
andy-dextrous <marketing@andrewscrivens.com.au>
2023-01-05 15:52:57 +1300

    Update webhook-request.js

--- src/api/webhook-request.js
+++ src/api/webhook-request.js
@@ -1,6 +1,33 @@
+const nodemailer = require("nodemailer")
+
 export default function handler(req, res) {
   const { body } = req
 
-  console.log(body)
+  // email body to me
+  const transporter = nodemailer.createTransport({
+    host: "mail.wildcreative.com.au",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "andrew@wildcreative.com.au",
+      pass: "R0bmach@d0",
+    },
+  })
+
+  const mailOptions = {
+    from: "andrew@wildcreative.com.au",
+    to: "andrew.scrivens@hotmail.com",
+    subject: "New Webhook Request",
+    text: JSON.stringify(body),
+  }
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log("Email sent: " + info.response)
+    }
+  })
+
   res.status(200).send({ ...body })
 }

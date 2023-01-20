025c4ec0c069bf78a427a6b3ca8ed458af7d5675
andy-dextrous <marketing@andrewscrivens.com.au>
2023-01-20 20:34:44 +1000

    Remove redundant files

--- src/gatsby-theme-wild-child/components/pages/HomepagePage.jsx
+++ src/gatsby-theme-wild-child/components/pages/HomepagePage.jsx
@@ -3,28 +3,12 @@ import React from "react"
 
 /*** Components ***/
 import Layout from "wildComponents/Layout"
-import CTA from "../../sections/homepageNew/CTA"
-import Hero from "../../sections/homepageNew/Hero"
-import Packages from "../../sections/homepageNew/Packages"
-import Quote from "../../sections/homepageNew/Quote"
-import Testimonials from "../../sections/homepageNew/Testimonials"
-import TrackRecord from "../../sections/homepageNew/TrackRecord"
-import WhatWeDo from "../../sections/homepageNew/WhatWeDo"
-import WorkingWithUs from "../../sections/homepageNew/WorkingWithUs"
-import Included from "../../sections/homepageNew/Included"
+import Hero from "../../sections/homepage/Hero"
 
 function HomepagePage() {
   return (
     <Layout>
       <Hero />
-      <Included />
-      <Quote />
-      <WhatWeDo />
-      <WorkingWithUs />
-      <Testimonials />
-      <TrackRecord />
-      <Packages />
-      <CTA />
     </Layout>
   )
 }
